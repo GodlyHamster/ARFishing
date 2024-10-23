2fafda66897148faad15d4b6758482256aad241a
GodlyHamster <wouter.van.dompselaar@gmail.com>
2024-10-23 16:51:59 +0200

    Bobber can be thrown in water

--- Assets/Scripts/TapInteraction.cs
+++ Assets/Scripts/TapInteraction.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
 
 public class TapInteraction : MonoBehaviour
 {
@@ -8,8 +10,44 @@ public class TapInteraction : MonoBehaviour
     private DebugText holding = new DebugText("Hold: False");
     private DebugText position = new DebugText("x:0, y:0");
 
+    [SerializeField]
+    private GameObject bobberPrefab;
+    [SerializeField]
+    private Material bobberPreviewMaterial;
+
+    [SerializeField]
+    private ARTrackedImageManager trackerManager;
+
+    private GameObject bobber;
+    private GameObject previewBobber;
+
+    private bool bobberInWater = false;
+
     private Vector3 tapLocation;
     private bool isHolding;
+
+    private void OnEnable()
+    {
+        trackerManager.trackedImagesChanged += OnChanged;
+    }
+
+    private void OnDisable()
+    {
+        trackerManager.trackedImagesChanged -= OnChanged;
+    }
+
+    private void OnChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var added in args.added)
+        {
+            Transform pondTransform = Pond.Instance.transform;
+            previewBobber = Instantiate(bobberPrefab, pondTransform.position, pondTransform.rotation, pondTransform);
+            previewBobber.GetComponentInChildren<MeshRenderer>().material = bobberPreviewMaterial;
+            bobber = Instantiate(bobberPrefab, pondTransform.position, pondTransform.rotation, pondTransform);
+            bobber.SetActive(false);
+            previewBobber.SetActive(false);
+        }
+    }
 
     private void Start()
     {
@@ -27,11 +65,28 @@ public class TapInteraction : MonoBehaviour
         {
             RaycastHit hit;
             Ray camRay = Camera.main.ScreenPointToRay(tapLocation);
-            Debug.DrawRay(camRay.origin, camRay.direction * 10, Color.red);
+            Debug.DrawRay(camRay.origin, camRay.direction * 10, Color.magenta);
             if (Physics.Raycast(camRay.origin, camRay.direction, out hit, Mathf.Infinity))
             {
-                holding.text = "Hold hit: " + hit.transform.gameObject.name;
+                bobberInWater = false;
+                Transform objectTransform = hit.transform;
+                previewBobber.transform.position = hit.point;
+                bobber.transform.position = hit.point;
+                bobber.SetActive(false);
+                previewBobber.SetActive(true);
             }
+        }
+        else
+        {
+            if (previewBobber != null)
+            {
+                if (previewBobber.activeSelf) bobberInWater = true;
+                previewBobber.SetActive(false);
+            }
+        }
+        if (bobberInWater)
+        {
+            bobber?.SetActive(true);
         }
     }
 
@@ -45,6 +100,7 @@ public class TapInteraction : MonoBehaviour
         isHolding = value.isPressed;
         holding.text = "Hold: " + isHolding;
     }
+
     private void OnTapLocation(InputValue value)
     {
         Vector2 pos = value.Get<Vector2>();
